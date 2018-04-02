1092f5e334f7216b3b8369f8a73eff0ba726a7ee
samussiah <samussiah@gmail.com>
2018-04-02 11:54:27 -0400

    rebuild

--- src/polyfills/math-log10.js
+++ src/polyfills/math-log10.js
@@ -1,3 +1,5 @@
-Math.log10 = Math.log10 = Math.log10 || function(x) {
-    return Math.log(x) * Math.LOG10E;
-};
+Math.log10 = Math.log10 =
+    Math.log10 ||
+    function(x) {
+        return Math.log(x) * Math.LOG10E;
+    };

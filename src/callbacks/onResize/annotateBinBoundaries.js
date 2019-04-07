5f92bab9978bb8fede10c59782f6fcad882a3609
Spencer Childress <samussiah@gmail.com>
2019-04-07 13:18:00 -0400

    fix #100

--- src/callbacks/onResize/annotateBinBoundaries.js
+++ src/callbacks/onResize/annotateBinBoundaries.js
@@ -1,60 +1,40 @@
 export default function annotateBinBoundaries() {
-    const context = this;
-
     //Remove bin boundaries.
-    this.svg.selectAll('.bin-boundary').remove();
+    this.svg.selectAll('text.bin-boundary').remove();
 
-    const binBoundaries = d3.set(
-        d3.merge(this.current_data.map(d => [this.config.x.d3format(d.rangeLow), this.config.x.d3format(d.rangeHigh)]))
-    ).values().sort((a,b) => +a - +b);
+    //Define set of bin boundaries.
+    const binBoundaries = d3
+        .set(d3.merge(this.current_data.map(d => [d.rangeLow, d.rangeHigh])))
+        .values()
+        .map(value => {
+            return {
+                value: +value,
+                value1: this.config.x.d3format(value),
+                value2: this.config.x.d3format1(value)
+            };
+        })
+        .sort((a, b) => a.value - b.value);
+
+    //Check for repeats of values formatted with lower precision.
+    const repeats = d3
+        .nest()
+        .key(d => d.value1)
+        .rollup(d => d.length)
+        .entries(binBoundaries)
+        .some(d => d.values > 1);
+
+    //Annotate bin boundaries.
     this.svg
         .selectAll('text.bin-boundary')
-            .data(binBoundaries)
-            .enter()
+        .data(binBoundaries)
+        .enter()
         .append('text')
         .classed('bin-boundary', true)
         .attr({
-            x: d => this.x(+d),
+            x: d => this.x(d.value),
             y: this.y(0),
             dy: '16px',
             'text-anchor': 'middle'
         })
-        .text(d => d);
-    //console.log(this.current_data);
-    //this.marks
-    //    .find(mark => mark.type === 'bar')
-    //    .groups
-    //    .each(function(d) {
-    //        //Annotate the lower boundary of the first bin.
-    //        const lower = context.config.x.d3format(d.rangeLow);
-    //        if (binBoundaries.indexOf(lower) < 0) {
-    //            console.log(`lower ${lower}`);
-    //            binBoundaries.push(lower);
-    //        }
-    //            const lowerBoundary = context.svg.append('text')
-    //                .classed('bin-boundary bin-boundary--lower', true)
-    //                .attr({
-    //                    'x': context.x(d.rangeLow),
-    //                    'y': context.y(0),
-    //                    'dy': '16px',
-    //                    'text-anchor': 'middle'
-    //                })
-    //                .text(lower);
-
-    //        //Annotate the upper boundary of all bins.
-    //        const upper = context.config.x.d3format(d.rangeHigh);
-    //        if (binBoundaries.indexOf(upper) < 0) {
-    //            console.log(`upper ${upper}`);
-    //            binBoundaries.push(upper);
-    //        }
-    //            const upperBoundary = context.svg.append('text')
-    //                .classed('bin-boundary bin-boundary--upper', true)
-    //                .attr({
-    //                    'x': context.x(d.rangeHigh),
-    //                    'y': context.y(0),
-    //                    'dy': '16px',
-    //                    'text-anchor': 'middle'
-    //                })
-    //                .text(upper);
-    //    });
+        .text(d => (repeats ? d.value2 : d.value1));
 }
